627208765f883924aa262eb4dcb446b113f8f5a9
muktesh <muktesh.kumar@tradus.com>
2014-01-17 13:33:13 +0530

    Updating signin html

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -1,19 +1,24 @@
 <?php
-
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
 ?>
-<html>
-<head><title>WELCOME TO LOGIN PAGE </title>
-<body style="background-color:skyblue;">
-<form method='post' name='process'>
-Username:<input type="text" name="email" align="right" />
-<p>
-Password:<input type="password" name="password" />
-<p>
-<input type="submit" name="submit" value="Log In" />
-</form>
-</body>
-</html>
+
+<div class="container">
+    <div class="row">
+        <div class="col-lg-4"></div>
+        <div class="col-lg-4">
+            <form class="form-signin" role="form">
+                <h2 class="form-signin-heading">Sign in </h2>
+                <input type="text" name="email" align="right" class="form-control" placeholder="Email address" required="" autofocus="">
+                <input type="password" name="password" class="form-control" placeholder="Password" required="">
+                <label class="checkbox">
+
+                </label>
+                <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+            </form>
+        </div>
+        <div class="col-lg-4"></div>
+    </div>
+</div>
